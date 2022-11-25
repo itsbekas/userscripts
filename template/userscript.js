80f628de0df105c3000728a80234617e12432d5e
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2022-11-25 16:38:05 +0000

    header + dev changes

---
+++ template/userscript.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         
+// @author       Bernardo Jordão
+// @match        
+// @grant        GM_log
+// @require      https://code.jquery.com/jquery-3.6.1.slim.min.js
+// @require      file://D:\Coding\Userscripts\...
+// ==/UserScript==
