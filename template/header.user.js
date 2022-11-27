e446b66cdf66daf9093751b53adf691f8b368dc4
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2022-11-27 00:17:17 +0000

    remove jquery dep
    if necessary, it'll be imported locally

--- template/header.user.js
+++ template/header.user.js
@@ -3,6 +3,5 @@
 // @author       Bernardo Jordão
 // @match        
 // @grant        GM_log
-// @require      https://code.jquery.com/jquery-3.6.1.slim.min.js
 // @require      https://raw.githubusercontent.com/itsbekas/userscripts/main/common/base.js
 // ==/UserScript==
--- web.budgetbakers.com/header.user.js
+++ web.budgetbakers.com/header.user.js
@@ -4,7 +4,6 @@
 // @match        https://web.budgetbakers.com/*
 // @grant        GM_xmlhttpRequest
 // @connect      127.0.0.1
-// @require      https://code.jquery.com/jquery-3.6.1.slim.min.js
 // @require      http://127.0.0.1:8080/common/base.js
-// @require      http://127.0.0.1:8080/web.budgetbakers.com/run.js
+// @require      http://127.0.0.1:8080/web.budgetbakers.com/inject.js
 // ==/UserScript==
