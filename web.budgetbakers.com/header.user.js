8eb82cf886d19bf0f3b3e6812af0321c8e5561c2
itsbekas <bernardo.jordao@tecnico.ulisboa.pt>
2022-11-30 14:27:57 +0000

    added custom filters option

--- web.budgetbakers.com/header.user.js
+++ web.budgetbakers.com/header.user.js
@@ -2,6 +2,7 @@
 // @name         Wallet by BudgetBakers (Dev)
 // @author       Bernardo Jordão
 // @match        https://web.budgetbakers.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=budgetbakers.com
 // @grant        GM_xmlhttpRequest
 // @connect      127.0.0.1
 // @require      http://127.0.0.1:8080/common/base.js
--- web.budgetbakers.com/records/elements.js
+++ web.budgetbakers.com/records/elements.js
@@ -11,14 +11,14 @@ function addToggleButton() {
 }
 
 function addCustomFilters() {
-    let customFilterAccordion = `<div class="accordion ui">
+    let customFilterAccordion = `<div class="accordion ui lUJGSX54wFqNxoJsJ2c2M _3uK5nO3_RKz7E4x-6oFDPW">
             <div class="title _37D2sJQ2lRixGNsvYXOWGd">
-                <i aria-hidden="true" class="dropdown icon">
-                    ::before
-                </i>
+                <i aria-hidden="true" class="dropdown icon"></i>
                 <span class="_3V5hQt-jNuvJMc3C5ahiNK">Custom Filters</span>
             </div>
         </div>`
+    let filterList = getElementsByClass("_26YpAPyiy2U5mYsxLuBGRZ")[0];
     let searchButton = getElementsByClass("_1uSKSaPiYsN9Ka7HhUnDlE")[0];
-    searchButton.after(elementFromString(customFilterAccordion));
+    let accountFilter = getElement("#root > div > div > main > div > div._5NFnhpp7joa9CQoFA2Fw- > div._14RWKiNFwtHCO_ubRsTZ57 > div:nth-child(3) > div > div:nth-child(4)");
+    filterList.insertBefore(elementFromString(customFilterAccordion), accountFilter);
 }
